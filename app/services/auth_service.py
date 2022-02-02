80aafc9eacb932800f23719c0a7fcfbf252953cb
kvv1618 <varun.kesharaju@gmail.com>
2022-02-02 23:20:27 +0530

    hashing completed

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -2,14 +2,15 @@ from app import db
 from app.schemas import UserLoginRequest,UserSignInRequest
 from app.models import User
 import jwt
+from passlib.context import CryptContext
 class AuthService:
-
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
     @classmethod
     def login(cls,request: UserLoginRequest):
          user = db.query(User).filter(User.email == request.email).first()
          if user is None:
            return {"message": "User not found"}
-         if user.hashed_password != request.password:
+         if not cls.pwd_context.verify(request.password, user.hashed_password):
             return {"message": "Invalid password"}
         
          return {"message": "Login Successful", "token": jwt.encode({"username": user.username, "email": user.email}, "secret")}
@@ -19,7 +20,7 @@ class AuthService:
         user = db.query(User).filter(User.email == request.email).first()
         if user is not None:
             return {"message": "User already exists"}
-        new_user=User(username=request.username,email=request.email,hashed_password=request.password)
+        new_user=User(username=request.username,email=request.email,hashed_password=cls.pwd_context.hash(request.password))
         db.add(new_user)
         db.commit()
-        return {"message": "User created"}
+        return {"message": new_user.hashed_password}
