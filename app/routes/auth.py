522bcacfa57cd19c0362ee60c7619df041af9d46
amr178 <amruth.pugal17@gmail.com>
2022-02-05 18:05:54 +0530

    added description/comments

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -6,7 +6,7 @@ from app.utils import authenticate_user
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
-
+"""End point for the registed users to login."""
 @router.post("/login")
 async def login(request: UserLoginRequest, response: Response):
     resp = await AuthService.login(request)
@@ -14,13 +14,14 @@ async def login(request: UserLoginRequest, response: Response):
     return resp
 
 
+"""End point for new users to register for the first time."""
 @router.post("/signup")
 async def signup(request: UserSignupRequest, response: Response):
     resp = await AuthService.signup(request)
     response.status_code = resp["status_code"]
     return resp
 
-
+"""End point to get the details of the logged in user."""
 @router.get("/jwt")
 async def jwt(user: UserResponse = Depends(authenticate_user)):
     return {"message": "Success", "data": user, "status_code": status.HTTP_200_OK}
